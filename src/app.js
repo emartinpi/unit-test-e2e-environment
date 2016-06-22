bc542751131ac797a5b952f655cf696814db71ac
Eduardo Martin <emartinpi@gmail.com>
2016-06-22 16:41:14 +0200

    feat(*): New style and functions for the main html page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,11 @@
+
+/**
+ * (Adds two numbers)
+ * 
+ * @param {Number} n1 
+ * @param {Number} n2
+ * @returns {Number}
+ */
 function add(n1, n2) {
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('Parameters must be numbers');
@@ -5,6 +13,13 @@ function add(n1, n2) {
     return n1 + n2;
 }
 
+/**
+ * (Subtract two numbers)
+ * 
+ * @param {Number} n1 
+ * @param {Number} n2
+ * @returns {Number}
+ */
 function subtract(n1, n2) {
     if (typeof n1 !== 'number' || typeof n2 !== 'number') {
         throw new Error('Parameters must be numbers');
@@ -12,14 +27,46 @@ function subtract(n1, n2) {
     return n1 - n2;
 }
 
-var btnAdd = document.querySelector('button#btnAdd');
-var btnSub = document.querySelector('button#btnSub');
-if (btnAdd && btnSub) {
-    btnAdd.addEventListener('click', function (ev) {
-        document.querySelector('span').textContent = add(1,2);
-    });
+/**
+ * (Multiply two numbers)
+ * 
+ * @param {Number} n1 
+ * @param {Number} n2
+ * @returns {Number}
+ */
+function multiply(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Parameters must be numbers');
+    }
+    return n1 * n2;
+}
 
-    btnSub.addEventListener('click', function (ev) {
-        document.querySelector('span').textContent = subtract(8,2);
+/**
+ * (Divide two numbers. The result is with two dicimals max)
+ * 
+ * @param {Number} n1 
+ * @param {Number} n2
+ * @returns {Number} (two decimals max)
+ */
+function divide(n1, n2) {
+    if (typeof n1 !== 'number' || typeof n2 !== 'number') {
+        throw new Error('Parameters must be numbers');
+    }
+
+    return Math.round(n1 / n2 * 100) / 100
+}
+
+/**
+ * Main
+ */
+var btn = document.querySelector('#calculate');
+if (btn) {
+    btn.addEventListener('click', function (ev) {
+        var n1 = +document.querySelector('#input1').value;
+        var n2 = +document.querySelector('#input2').value;
+        document.querySelector('#resAdd').innerHTML =  n1 + ' <span style="color:green">+</span> ' + n2 + ' = ' +  add(n1, n2);
+        document.querySelector('#resSub').innerHTML =  n1 + ' <span style="color:orange">-</span> ' + n2 + ' = ' +  subtract(n1, n2);
+        document.querySelector('#resMul').innerHTML =  n1 + ' <span style="color:blue">*</span> ' + n2 + ' = ' +  multiply(n1, n2);
+        document.querySelector('#resDiv').innerHTML =  n1 + ' <span style="color:red">/</span> ' + n2 + ' = ' +  divide(n1, n2);
     });
 }
